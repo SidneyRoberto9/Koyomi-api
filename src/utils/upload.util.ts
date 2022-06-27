5069ae9ee7f3cb6c7bebd112cac3e7bdfdf79e1d
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-06-26 21:42:23 -0300

    🧪 test: error handle

--- src/utils/upload.util.ts
+++ src/utils/upload.util.ts
@@ -5,6 +5,7 @@ import config from '../config';
 
 export const uploadFile = async (file: any) => {
   const GOOGLE_FOLDER_ID = config.GOOGLE_FOLDER_ID;
+  console.log(1);
 
   try {
     const auth = new google.auth.GoogleAuth({
@@ -15,30 +16,30 @@ export const uploadFile = async (file: any) => {
       },
       scopes: ['https://www.googleapis.com/auth/drive'],
     });
-
+    console.log(2);
     const driveService = google.drive({
       version: 'v3',
       auth,
     });
-
+    console.log(3);
     const fileMetadata = {
       name: file.filename,
       parents: [GOOGLE_FOLDER_ID],
     };
-
+    console.log(4);
     const media = {
       mimeType: file.mimeType,
       body: fs.createReadStream(file.path),
     };
-
+    console.log(5);
     const response = await driveService.files.create({
       requestBody: fileMetadata,
       media: media,
       fields: 'id',
     });
-
+    console.log(6);
     fs.unlinkSync(file.path);
-
+    console.log(7);
     return `https://drive.google.com/uc?export=view&id=${response.data.id}`;
   } catch (error) {
     console.log('Erro in create file');
