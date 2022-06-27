360488d672c380fbc5a93b93bf85e95ec2c4b011
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-06-27 08:34:47 -0300

    🗑️ revert: test keyfile

--- src/utils/upload.util.ts
+++ src/utils/upload.util.ts
@@ -2,14 +2,16 @@ import fs from 'fs';
 import { google } from 'googleapis';
 
 import config from '../config';
-import { __baseDir } from './../server';
 
 export const uploadFile = async (file: any) => {
   const GOOGLE_FOLDER_ID = config.GOOGLE_FOLDER_ID;
-  const KEYFILE = __baseDir + '/utils/GoogleDrive.json';
 
   const auth = new google.auth.GoogleAuth({
-    keyFile: KEYFILE,
+    credentials: {
+      private_key: config.GOOGLE_PRIVATE_KEY,
+      client_email: config.GOOGLE_EMAIL,
+      client_id: config.GOOGLE_CLIENT_ID,
+    },
     scopes: ['https://www.googleapis.com/auth/drive'],
   });
 
@@ -40,6 +42,6 @@ export const uploadFile = async (file: any) => {
 
     return `https://drive.google.com/uc?export=view&id=${response.data.id}`;
   } catch (error) {
-    throw error.message;
+    console.log('Erro in create file');
   }
 };
