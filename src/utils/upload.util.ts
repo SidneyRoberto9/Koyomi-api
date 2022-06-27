7c4485e7e2873252d156d62217c9bf277c40b050
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-06-26 21:51:22 -0300

    📦 refactor: change mimeType file

--- src/utils/upload.util.ts
+++ src/utils/upload.util.ts
@@ -5,7 +5,6 @@ import config from '../config';
 
 export const uploadFile = async (file: any) => {
   const GOOGLE_FOLDER_ID = config.GOOGLE_FOLDER_ID;
-  console.log(1);
 
   try {
     const auth = new google.auth.GoogleAuth({
@@ -14,24 +13,29 @@ export const uploadFile = async (file: any) => {
         client_email: config.GOOGLE_EMAIL,
         client_id: config.GOOGLE_CLIENT_ID,
       },
-      scopes: ['https://www.googleapis.com/auth/drive'],
+      scopes: [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.appdata',
+        'https://www.googleapis.com/auth/drive.file',
+      ],
     });
-    console.log(2);
+
     const driveService = google.drive({
       version: 'v3',
       auth,
     });
-    console.log(3);
+
     const fileMetadata = {
       name: file.filename,
       parents: [GOOGLE_FOLDER_ID],
+      mimeType: 'application/vnd.google-apps.file',
     };
-    console.log(4);
+
     const media = {
       mimeType: file.mimeType,
       body: fs.createReadStream(file.path),
     };
-    console.log(5);
+
     const response = await driveService.files.create({
       requestBody: fileMetadata,
       media: media,
