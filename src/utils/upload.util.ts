6260648bf2357658767283e4e433300eeadc6ac7
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-06-26 21:38:51 -0300

    🐛 fix: using file.path

--- src/utils/upload.util.ts
+++ src/utils/upload.util.ts
@@ -1,13 +1,10 @@
 import fs from 'fs';
 import { google } from 'googleapis';
-import path from 'path';
 
 import config from '../config';
-import { __baseDir } from '../server';
 
 export const uploadFile = async (file: any) => {
   const GOOGLE_FOLDER_ID = config.GOOGLE_FOLDER_ID;
-  const FILE_PATH = __baseDir + '/tmp/' + file.filename;
 
   try {
     const auth = new google.auth.GoogleAuth({
@@ -31,7 +28,7 @@ export const uploadFile = async (file: any) => {
 
     const media = {
       mimeType: file.mimeType,
-      body: fs.createReadStream(FILE_PATH),
+      body: fs.createReadStream(file.path),
     };
 
     const response = await driveService.files.create({
@@ -40,7 +37,7 @@ export const uploadFile = async (file: any) => {
       fields: 'id',
     });
 
-    fs.unlinkSync(path.join(FILE_PATH));
+    fs.unlinkSync(file.path);
 
     return `https://drive.google.com/uc?export=view&id=${response.data.id}`;
   } catch (error) {
