e52275308be41d7044a9ec76a04462a897eac3cd
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-06-26 22:03:30 -0300

    📦 refactor: change try scope

--- src/utils/upload.util.ts
+++ src/utils/upload.util.ts
@@ -6,36 +6,35 @@ import config from '../config';
 export const uploadFile = async (file: any) => {
   const GOOGLE_FOLDER_ID = config.GOOGLE_FOLDER_ID;
 
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      private_key: config.GOOGLE_PRIVATE_KEY,
+      client_email: config.GOOGLE_EMAIL,
+      client_id: config.GOOGLE_CLIENT_ID,
+    },
+    scopes: [
+      'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/drive.appdata',
+      'https://www.googleapis.com/auth/drive.file',
+    ],
+  });
+
+  const driveService = google.drive({
+    version: 'v3',
+    auth,
+  });
+
+  const fileMetadata = {
+    name: file.filename,
+    parents: [GOOGLE_FOLDER_ID],
+  };
+
+  const media = {
+    mimeType: file.mimeType,
+    body: fs.createReadStream(file.path),
+  };
+
   try {
-    const auth = new google.auth.GoogleAuth({
-      credentials: {
-        private_key: config.GOOGLE_PRIVATE_KEY,
-        client_email: config.GOOGLE_EMAIL,
-        client_id: config.GOOGLE_CLIENT_ID,
-      },
-      scopes: [
-        'https://www.googleapis.com/auth/drive',
-        'https://www.googleapis.com/auth/drive.appdata',
-        'https://www.googleapis.com/auth/drive.file',
-      ],
-    });
-
-    const driveService = google.drive({
-      version: 'v3',
-      auth,
-    });
-
-    const fileMetadata = {
-      name: file.filename,
-      parents: [GOOGLE_FOLDER_ID],
-      mimeType: 'application/vnd.google-apps.file',
-    };
-
-    const media = {
-      mimeType: file.mimeType,
-      body: fs.createReadStream(file.path),
-    };
-
     const response = await driveService.files.create({
       requestBody: fileMetadata,
       media: media,
@@ -43,7 +42,7 @@ export const uploadFile = async (file: any) => {
     });
     console.log(6);
     fs.unlinkSync(file.path);
-    console.log(7);
+
     return `https://drive.google.com/uc?export=view&id=${response.data.id}`;
   } catch (error) {
     console.log('Erro in create file');
